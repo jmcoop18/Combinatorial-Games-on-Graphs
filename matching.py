6709c881e1236158a8b8bda7b30494e365667621
jmcoop18 <jmcoop18@gmail.com>
2026-07-14 14:25:15 -0400

    vazarani

--- matching.py
+++ matching.py
@@ -11,7 +11,8 @@ import networkx as nx
 # INPUT:  Graph G, initial matching M on G
 # OUTPUT: maximum matching M* on G
 def find_maximum_matching(G, M):
-    P = find_augmenting_path(G, M) 
+    # P = find_augmenting_path(G, M) # my algorithm
+    P = find_augmenting_path_micali_vazirani(G, M)
 
     if P == []:  # Base case
         return M
